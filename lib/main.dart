dd5d1f294d8221d06d6bfbe23d3e1f8e3aecedff
unknown <emmanueluchechukwu107@gmail.com>
2022-12-18 10:59:22 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController otpController = TextEditingController();
+
+  String userNumber = '';
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  var otpFieldVisibility = false;
+  var receivedID = '';
+
+  void verifyUserPhoneNumber() {
+    auth.verifyPhoneNumber(
+      phoneNumber: userNumber,
+      verificationCompleted: (PhoneAuthCredential credential) async {
+        await auth.signInWithCredential(credential).then(
+              (value) => print('Logged In Successfully'),
+            );
+      },
+      verificationFailed: (FirebaseAuthException e) {
+        print(e.message);
+      },
+      codeSent: (String verificationId, int? resendToken) {
+        receivedID = verificationId;
+        otpFieldVisibility = true;
+        setState(() {});
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {},
+    );
+  }
+
+  Future<void> verifyOTPCode() async {
+    PhoneAuthCredential credential = PhoneAuthProvider.credential(
+      verificationId: receivedID,
+      smsCode: otpController.text,
+    );
+    await auth
+        .signInWithCredential(credential)
+        .then((value) => print('User Login In Successful'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Phone Authentication',
+          ),
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: IntlPhoneField(
+                controller: phoneController,
+                initialCountryCode: 'NG',
+                decoration: const InputDecoration(
+                  hintText: 'Phone Number',
+                  labelText: 'Phone',
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (val) {
+                  userNumber = val.completeNumber;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Visibility(
+                visible: otpFieldVisibility,
+                child: TextField(
+                  controller: otpController,
+                  decoration: const InputDecoration(
+                    hintText: 'OTP Code',
+                    labelText: 'OTP',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (otpFieldVisibility) {
+                  verifyOTPCode();
+                } else {
+                  verifyUserPhoneNumber();
+                }
+                FocusManager.instance.primaryFocus?.unfocus();
+              },
+              child: Text(
+                otpFieldVisibility ? 'Login' : 'Verify',
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
